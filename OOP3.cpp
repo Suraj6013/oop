161e06a6ac580b5d73cac2e97e36380eb68a16ed
Suraj6013 <surajwadkar2021.ainds@mmcoe.edu.in>
2024-05-24 22:42:01 +0530

    Refactor OOP3.cpp and OOP5.cpp for improved readability

--- OOP3.cpp
+++ OOP3.cpp
@@ -1,4 +1,4 @@
-
+/*
 OOP Assignment 03
 Title:-Imagine a publishing company which does marketing for book and audiocassette versions.
 Create a class publication that stores the title (a string) and price (type float) of a publication.
@@ -7,165 +7,64 @@ adds a playing time in minutes (type float).Write a program that instantiates th
 classes, allows user to enter data and displays the data members. If an exception is caught,
 replace all the data member values with zero values.
 ****************************************************************************/
-//Program-
+
 #include<iostream>
-#include<cstring>
 #include<exception>
 using namespace std;
-class publication{
+
+class Publication {
 protected:
-string title;
-float price;
+    string title;
+    float price;
 public:
-publication(string t = "No title", float p = 0.0)
-{
-title=t;
-price=p;
-}
+    Publication(string t = "No title", float p = 0.0) : title(t), price(p) {}
+    virtual void getData() = 0;  // Pure virtual function
+    virtual void display() = 0;  // Pure virtual function
 };
-class books:public publication
-{int pages;
+
+class Book : public Publication {
+    int pages;
 public:
-books(string t="No title",float p= 0.0, int pa = 0):publication(t,p)
-{
-pages=pa;
-}
-void getb()
-{
-cout<<"Enter the datails of the book :"<<endl;
-cout<<"Enter the title of the book :"<<endl;
-cin>>title;
-//cin.ignore(1,'\n');
-//getline(cin,title);
-cout<<"Enter the price of the book :"<<endl;
-cin>>price;
-cout<<"Enter the pages of the book :"<<endl;
-cin>>pages;
-try
-{
-if(pages > 500 && pages <1500)
-{
-if(price >100 && price < 2000)
-{
-displayb();
-}
-}
-else throw(pages);
-}
-catch(int i){
-cout<<"Caught exception in function of book"<<endl;
-cout<<"You entered an invalid data"<<endl;title = "0";
-price = 0.0;
-pages = 0;
-displayb();
-throw;
-}
-}
-void displayb()
-{
-cout<<"The book you entered is :"<<endl<<endl;
-cout<<"The entered title is :"<<title<<endl;
-cout<<"The entered price is :"<<price<<endl;
-cout<<"The entered pages are :"<<pages<<endl;
-}
+    Book(string t="No title", float p= 0.0, int pa = 0) : Publication(t,p), pages(pa) {}
+    void getData() override {
+        cout << "Enter the title, price and pages of the book: ";
+        cin >> title >> price >> pages;
+    }
+    void display() override {
+        cout << "Book: " << title << ", Price: " << price << ", Pages: " << pages << endl;
+    }
 };
-class tape:public publication
-{
-float min;
+
+class Tape : public Publication {
+    float min;
 public:
-tape(string t="No title",float p= 0.0, float m = 0.00):publication(t,p)
-{
-min=m;
+    Tape(string t="No title", float p= 0.0, float m = 0.0) : Publication(t,p), min(m) {}
+    void getData() override {
+        cout << "Enter the title, price and playing time of the tape: ";
+        cin >> title >> price >> min;
+    }
+    void display() override {
+        cout << "Tape: " << title << ", Price: " << price << ", Time: " << min << endl;
+    }
+};
+
+int main() {
+    Book b;
+    Tape t;
+    int choice;
+    cout << "Do you want to buy a book (Press 1) or a tape (Press 2): ";
+    cin >> choice;
+    switch(choice) {
+        case 1: 
+            b.getData();
+            b.display();
+            break;
+        case 2: 
+            t.getData();
+            t.display();
+            break;
+        default:
+            cout << "Invalid choice! Try again!" << endl;
+    }
+    return 0;
 }
-void gett()
-{
-cout<<"Enter the details of the tape :"<<endl;
-cout<<"Enter the title of the tape :"<<endl;
-cin>>title;
-//cin.ignore(1,'\n');
-//getline(cin,title);
-cout<<"Enter the price of the tape :"<<endl;cin>>price;
-cout<<"Enter the pages of the playing time(in minutes) :"<<endl;
-cin>>min;
-try
-{
-if(min > 30.00 && min < 90.00)
-{
-if(price > 500 && price < 1000)
-{
-displayt();
-}
-}
-else throw(min);
-}
-catch(float f){
-cout<<"caught exception in the function of tape"<<endl;
-cout<<"you entered an invalid data"<<endl;
-title = "0";
-price = 0.0;
-min = 0.0;
-displayt();
-throw;
-}
-}
-void displayt()
-{
-cout<<"The details of tape you entered is :"<<endl<<endl;
-cout<<"The entered title is :"<<title<<endl;
-cout<<"The entered price is :"<<price<<endl;
-cout<<"The entered play time is :"<<min<<endl;
-}};
-int main()
-{
-books b;
-tape t;
-int choice;
-cout<<"Do want to buy a book(Press 1) or a tape(Press 2) :"<<flush;
-cin>>choice;
-switch(choice)
-{
-case 1: try
-{
-b.getb();
-}
-catch(int i){
-cout<<"Caught exception in int main()"<<endl;
-}
-break;
-case 2: try
-{
-t.gett();
-}
-catch(float f){
-cout<<"Caught exception in int main()"<<endl;
-}
-break;
-default:cout<<"Entered bad choice!! Try again!!"<<endl;
-}
-return 0;
-}
-/*Output:-system@system-Lenovo:~$ g++ filename.cpp
-system@system-Lenovo:~$ ./a.out
-Do want to buy a book(Press 1) or a tape(Press 2) :1
-Enter the details of the book :
-Enter the title of the book :the man
-Enter the price of the book :200
-Enter the pages of the book :366
-Caught exception in function of book
-You entered an invalid data
-The book you entered is :
-The entered title is :0
-The entered price is :0
-The entered pages are :0
-Caught exception in int main()
-system@system-Lenovo:~$ ./a.out
-Do want to buy a book(Press 1) or a tape(Press 2) :2
-Enter the details of the tape :
-Enter the title of the tape :tape real
-Enter the price of the tape :600
-Enter the pages of the playing time(in minutes) :80
-The details of tape you entered is :
-The entered title is :tape real
-The entered price is :600
-The entered play time is :80
-*/
